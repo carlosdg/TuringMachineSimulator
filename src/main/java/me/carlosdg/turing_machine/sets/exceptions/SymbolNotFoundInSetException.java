a5a7475f94fb86ff126cf46563cec3182df2b725
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-05 20:15:22 +0000

    Refactor: SymbolNotFoundInSetException
    Added getters for the information of the exception because we will need them soon

--- src/main/java/me/carlosdg/turing_machine/sets/exceptions/SymbolNotFoundInSetException.java
+++ src/main/java/me/carlosdg/turing_machine/sets/exceptions/SymbolNotFoundInSetException.java
@@ -12,14 +12,29 @@ public class SymbolNotFoundInSetException extends Exception {
 
 	private static final long serialVersionUID = 7250455007229293610L;
 
+	private String stringReprOfNotFoundSymbol;
+	private SymbolSet<?> set;
+
 	/**
 	 * Creates an instance of this exception
 	 *
 	 * @param stringReprOfNotFoundSymbol String representation of the symbol that
 	 *                                   wasn't found in the alphabet
+	 * @param set                        Set where the symbol wasn't found
 	 */
-	public SymbolNotFoundInSetException(String stringReprOfNotFoundSymbol, SymbolSet<?> stringReprOfSet) {
-		super("Symbol '" + stringReprOfNotFoundSymbol + "' in not found in " + stringReprOfSet);
+	public SymbolNotFoundInSetException(String stringReprOfNotFoundSymbol, SymbolSet<?> set) {
+		super("Symbol '" + stringReprOfNotFoundSymbol + "' in not found in " + set);
+
+		this.stringReprOfNotFoundSymbol = stringReprOfNotFoundSymbol;
+		this.set = set;
+	}
+
+	public String getStringReprOfNotFoundSymbol() {
+		return stringReprOfNotFoundSymbol;
+	}
+
+	public SymbolSet<?> getSet() {
+		return set;
 	}
 
 }
